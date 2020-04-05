1a391980709a1a1388d19361a3075390f5d8cb16
Juraj Lazorík <44274611+duri488@users.noreply.github.com>
2020-04-05 16:55:08 +0200

    Implemented mathlib functions

--- src/mathlib.py
+++ src/mathlib.py
@@ -1,25 +1,42 @@
 #definitions of math functions for calculator
 
 def add(a,b):
-    pass
+    return a+b
 
 def subtraction(a,b):
-    pass
+    return a-b
 
 def multiplication(a,b):
-    pass
+    return a*b
 
 def divide(a,b):
-    pass
+    if b == 0:
+        return "Invalid input"
+    return a/b
 
 def modulo(a,b):
-    pass
+    return a%b
 
-def power(base,x):
-    pass
+def power(base,x): #format base^x
+    return base**x
 
-def nthRoot(x,base):
-    pass
+def nthRoot(x,base): #format x-th root from base
+
+    if base < 0 and x%2 == 0:
+        return "Invalid input"
+
+    if x == 0:
+        return"Invalid input"
+
+    return (base)**(1/x)
     
-def factorial(n):
-    pass
+#@strict_types
+def factorial(n: int) -> int:
+    if n < 0:
+        return "Invalid input"
+    elif n == 0:
+        return 1
+    else:
+        for i in range(1,n + 1):
+            factorial = factorial*i
+        return factorial
